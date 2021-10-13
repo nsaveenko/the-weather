c634fa177c92053c76c079dd62c52d908ece2904
nsaveenko <nn.saveenko@gmail.com>
2021-10-13 11:31:57 +0300

    fix: change api

--- src/main.js
+++ src/main.js
@@ -4,39 +4,43 @@ const temp = document.querySelector(".temp");
 const description = document.querySelector(".weather-descripton")
 const humidity = document.querySelector(".humidity-value");
 const pressure = document.querySelector(".pressure-value");
-const weatherIconSrc = document.querySelector(".icon").src;
 const searchButton = document.querySelector(".search-button");
 const searchBar = document.querySelector(".search-bar");
+const sunrise = document.querySelector(".sunrise-time-value");
+const sunset = document.querySelector(".sunset-time-value");
 
 const weather = {
-    apiKey: "c0fec93c79e38fa329d29aa7ddf6849a",
-    fetchWeather: function(city = "Minsk") {
-        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + this.apiKey)
+    apiKey: "9baa190c5b0f4f81825171901211210",
+    fetchWeather: function(city = "Minsk", daysCount = 3) {
+        fetch("https://api.weatherapi.com/v1/forecast.json?key=" + this.apiKey + "&q=" + city + "&days=" + daysCount + "&aqi=no&alerts=no")
             .then(response => response.json())
             .then(data => this.displayWeather(data));
     },
     displayWeather: function(data) {
         const currentWeather = {
-            cityName: data.name,
-            description: data.weather[0].main,
-            temp: Math.round(data.main.temp),
-            windSpeed: Math.round(data.wind.speed),
-            humidity: data.main.humidity,
-            pressure: data.main.pressure
+            cityName: data.location.name,
+            description: data.current.condition.text,
+            temp: Math.round(data.current.temp_c),
+            windSpeed: data.current.wind_kph,
+            humidity: data.current.humidity,
+            pressure: data.current.pressure_mb,
+            iconSrc: data.current.condition.icon,
+            sunrise: data.forecast.forecastday[0].astro.sunrise,
+            sunset: data.forecast.forecastday[0].astro.sunset,
         }
 
-        const {icon} = data.weather[0];
-
+        sunrise.innerHTML = currentWeather.sunrise;
+        sunset.innerHTML = currentWeather.sunset;
         cityTitle.innerHTML = currentWeather.cityName;
         windSpeed.innerHTML = currentWeather.windSpeed + " km/h";
         temp.innerHTML = currentWeather.temp + "°";
         description.innerHTML = currentWeather.description;
         humidity.innerHTML = currentWeather.humidity + " %";
         pressure.innerHTML = currentWeather.pressure + " mb"; 
-        document.querySelector(".icon").src = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+        document.querySelector(".icon").src = currentWeather.iconSrc;
     },
     search: function () {
-        this.fetchWeather(searchBar.value)
+        this.fetchWeather(searchBar.value);
         searchBar.value = "";
     }
 }
