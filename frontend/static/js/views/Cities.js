783a86c0b519c3d59ff4122ea9607148c5ea34f8
nsaveenko <nn.saveenko@gmail.com>
2021-10-14 15:13:52 +0300

    feat: add router

---
+++ frontend/static/js/views/Cities.js
@@ -0,0 +1,14 @@
+import AbstactView from './AbstractView.js';
+
+export default class extends AbstactView {
+    constructor(params) {
+        super(params);
+        this.setTitle('The Weather App');
+    }
+
+    async getHtml() {
+        return `
+        <h2>cities</h2>
+        `;
+    }
+}
